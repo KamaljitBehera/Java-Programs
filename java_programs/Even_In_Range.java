678697b52d895960f70f9ec1304d038bbf6dc47a
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-19 11:19:47 +0530

    Even_In_Range program added successfully

---
+++ java_programs/Even_In_Range.java
@@ -0,0 +1,21 @@
+package java_programs;
+
+import java.util.Scanner;
+
+public class Even_In_Range {
+  public static void main(String[] args) {
+ Scanner sc= new Scanner(System.in);
+	//taking the range from user
+		System.out.println("Enter the range::");
+		int num=sc.nextInt();
+		//to print the even number in the range we have to use for loop
+		for(int i=0;i<num;i++) {
+			if(i%2==0) {
+				System.out.print(i+" ");
+			}
+		}
+		
+sc.close();
+}
+}
+
