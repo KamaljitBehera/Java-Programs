e5dc0c162b3d083de8f2f902f2a5232f9a1c92a0
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-23 21:28:15 +0530

    Default_Constructor program  added successfully

---
+++ java_programs/Default_Constructor.java
@@ -0,0 +1,15 @@
+package java_programs;
+
+//This program is used to demonstrate the Default constructor concept
+
+     
+public class Default_Constructor {
+  //here we not create any constructor explicitly
+	public static void main(String args[]) {
+		
+	
+	 Default_Constructor obj=new Default_Constructor();
+	 //After creating a object JVM check whether a constructor is present in current class or not.
+	 //if not then it creates a default constructor to initialize object
+	}
+}
