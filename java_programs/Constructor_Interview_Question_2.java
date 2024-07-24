0a615a72a2db758be6f3f66f7ff607b5e208a99f
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-24 08:57:36 +0530

    Constructor_Interview_Question_2 added successfully

---
+++ java_programs/Constructor_Interview_Question_2.java
@@ -0,0 +1,18 @@
+package java_programs;
+
+class Test
+{
+	int num=10;
+	Test(int num1){
+		num=num1;
+		System.out.println(num);
+	}
+}
+
+public class Constructor_Interview_Question_2 {
+  public static void main(String[] args) {
+	  //creating test class object and passing 40 as an argument
+	Test obj=new Test(40);
+	//guess the output
+}
+}
