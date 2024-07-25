d6fe051850c8ca766eba7792eaacece7e3f07399
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-25 22:07:39 +0530

    Happy_Number program added successfully

---
+++ java_programs/Happy_Number.java
@@ -0,0 +1,41 @@
+package java_programs;
+
+import java.util.Scanner;
+
+public class Happy_Number {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		int num=n;
+        int sum=0,rem=0,count=0;
+
+		while(sum!=1)
+		{   
+			if(count<=4)
+			{
+				sum=0;
+				while(n>0)
+				{
+                  rem=n%10;
+				  sum=sum+(rem*rem);
+				  n=n/10;
+				}
+				n=sum;
+				count++;
+				if(sum==1)
+				{
+					System.out.println("happy no.");
+				}
+			}
+			else
+			{
+				System.out.println("not a happy no.");
+				break;
+			}
+			//n=sum;
+		}
+	}
+
+}
