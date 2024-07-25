1656e9c11596868cbbd87509e71ad825b611e90a
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-25 22:10:41 +0530

    Single_Inheritance program added successfully

---
+++ java_programs/Single_Inheritance.java
@@ -0,0 +1,26 @@
+package java_programs;
+
+//Java Program to illustrate Inheritance (concise)
+
+//Base or Super Class
+	class Employee {
+	    int salary = 60000;
+	}
+	
+	
+	// Inherited or Sub Class
+		class Engineer extends Employee {
+		    int benefits = 10000;
+		}
+		// Through this class we show the single inheritance
+public class Single_Inheritance{
+	    public static void main(String args[])
+	    {
+	    	//creating the child class object
+	    	//By using the inheritance concept we can access the parent class members
+	        Engineer E1 = new Engineer();
+	        System.out.println("Salary : " + E1.salary
+	                           + "\nBenefits : " + E1.benefits);
+	    }
+}
+
