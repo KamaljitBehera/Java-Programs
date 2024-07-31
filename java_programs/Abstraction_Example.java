bff2ed068a677ac95b32523c4bda8ae2f2823e2c
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-31 10:07:53 +0530

    Abstraction_Example added successfuly

---
+++ java_programs/Abstraction_Example.java
@@ -0,0 +1,48 @@
+package java_programs;
+//This program is used to demonstrate how to use the abstract class 
+
+// Let's create an abstract class Shape
+abstract class Shape{
+	String name;
+	String color;
+//	creating the constructor to initialize the variable
+	public Shape(String name,String color) {
+		this.color=color;
+		this.name=name;
+	}
+	//cfeating an abstract method
+	public abstract void area(int width,int height) ;
+//	create an no abstract method showing that an abstract class both have abstract as well as concrete method
+	public void details(){
+		System.out.println("This is in "+name+" shape");
+		System.out.println("The color of the shapee is "+color);
+	}
+}
+
+//creating an child class of Shape
+class Rectangle extends Shape{
+//       creating the parameterized constructor having name and color as an parameter
+	public Rectangle(String name, String color) {
+		super(name, color);
+		System.out.println("Hello there");
+		// TODO Auto-generated constructor stub
+	}
+// overriding the area method
+	@Override
+	public void area(int width, int height) {
+		System.out.println("The area of the shape is "+(width*height));
+		
+	}
+	
+}
+public class Abstraction_Example {
+  public static void main(String[] args) {
+	
+//	 Creating the object of Rectangle class
+	  
+	  Rectangle rc=new Rectangle("Rectangle","Red");
+//	  Caling the methods
+	  rc.area(10,20);
+	  rc.details();
+}
+}
