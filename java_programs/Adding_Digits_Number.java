cda4ec67421ff5bfacaa34c0eed4ba2621085769
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-07-31 10:11:11 +0530

    Adding_Digits_Number and Basic program added successfuly

--- java_programs/Adding_Digits_Number.java
+++ java_programs/Adding_Digits_Number.java
@@ -1,27 +1,28 @@
 package java_programs;
 
+// This program is used to add the digits of an number
 import java.util.Scanner;
 
 public class Adding_Digits_Number {
-  public static void main(String[] args) {
-		  System.out.println("Hello there! Lets add the digit of the number you have entered");
-		  //taking input from user
-		  System.out.println("Enter the number:");
-		  Scanner sc=new Scanner(System.in);
-		  int num=sc.nextInt();
-		  int reminder=0,sum=0;;
-		  
-		  //for adding the digits of  a number we have use the while loop here
-		  while(num>1) {
-			  //reminder to store the left
-			  reminder=num%10;
-			  System.out.println(reminder);
-			  sum=reminder+sum;
-			  //to reduce the number
-			  num=num/10;
-		  }
-		  //print the sum of digit of the given number
-		  System.out.println("Sum of the digit of given number is::"+(sum));
-		  sc.close();
-}
+	public static void main(String[] args) {
+		System.out.println("Hello there! Lets add the digit of the number you have entered");
+		// taking input from user
+		System.out.println("Enter the number:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int reminder = 0, sum = 0;
+
+		// for adding the digits of a number we have use the while loop here
+		while (num > 1) {
+			// reminder to store the left
+			reminder = num % 10;
+			System.out.println(reminder);
+			sum = reminder + sum;
+			// to reduce the number
+			num = num / 10;
+		}
+		// print the sum of digit of the given number
+		System.out.println("Sum of the digit of given number is::" + (sum));
+		sc.close();
+	}
 }
--- java_programs/Basic.java
+++ java_programs/Basic.java
@@ -1,27 +1,29 @@
 package java_programs;
 
 public class Basic {
-	//A simple program to understand the components of class
-		//which has some state(variables), behavior(methods),identity(unique name),methods
-		//here create two static variable
-		    static String Employee_name;
-		    static float Employee_salary;
-	        //here create a method to initializing the variables
-		    static void set(String n, float p) {
-		        Employee_name  = n;
-		        Employee_salary  = p;
-		    }
-	         //here create a method to get the value of the variables
-		    static void get() {
-		        System.out.println("Employee name is: " +Employee_name );
-		        System.out.println("Employee CTC is: " + Employee_salary);
-		    }
+	// A simple program to understand the components of class
+	// which has some state(variables), behavior(methods),identity(unique
+	// name),methods
+	// here create two static variable
+	static String Employee_name;
+	static float Employee_salary;
 
-		    public static void main(String args[]) {
-		    	//call the two methods
-		        Basic.set("Rathod Avinash", 10000.0f);
-		        Basic.get();
-		    }
+	// here create a method to initializing the variables
+	static void set(String n, float p) {
+		Employee_name = n;
+		Employee_salary = p;
+	}
 
+	// here create a method to get the value of the variables
+	static void get() {
+		System.out.println("Employee name is: " + Employee_name);
+		System.out.println("Employee CTC is: " + Employee_salary);
+	}
+
+	public static void main(String args[]) {
+		// call the two methods
+		Basic.set("Rathod Avinash", 10000.0f);
+		Basic.get();
+	}
 
 }
