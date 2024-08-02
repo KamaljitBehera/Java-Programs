908667aa6d723bcfe14034b89e91ab80d81ba365
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-02 09:48:34 +0530

    Abstraction test case 2 program added successfuly

---
+++ java_test/Abstraction_2.java
@@ -0,0 +1,19 @@
+package java_test;
+// This program is used to demonstrate that can we give implementation to abstract class directly
+
+//Creating an abstract class
+abstract class Test1{
+	//crating an abstract method with implementation
+/*
+ * public abstract void details{ 
+ * System.out.println("Hello there i am the abstract class having my own implementation");
+ * }
+ */ //here we get compile time error
+	
+//	we cannot provide implementation to the abstract method
+}
+public class Abstraction_2 {
+   public static void main(String[] args) {
+	
+}
+}
