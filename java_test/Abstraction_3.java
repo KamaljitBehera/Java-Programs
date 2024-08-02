a85360789e503026b2ccbf3e0c6db3258c87bfa1
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-02 09:49:10 +0530

    Abstraction test case 3 program added successfuly

---
+++ java_test/Abstraction_3.java
@@ -0,0 +1,16 @@
+package java_test;
+
+// This program is demonstrate that can we provide access modifier private to any abstract method or not
+public abstract class Abstraction_3 {
+	// creating the abstract method
+	// private abstract void details();//here we get compile time error
+
+	/*
+	 * Explanation:- We cannot declare an abstract method as private because if we
+	 * declare the we cannot override the method in the implemented class
+	 */
+
+	public static void main(String[] args) {
+
+	}
+}
