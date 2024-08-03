af1437baceffa1aa0dcf22c91d3d09f933baea1d
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-03 09:41:18 +0530

    Abstraction_6 program added successfuly

---
+++ java_test/Abstraction_6.java
@@ -0,0 +1,18 @@
+package java_test;
+//This program is used to check whether a normal class can have abstract method or not 
+
+//here Abstraction_6 is a normal class
+public class Abstraction_6 {
+	//creating an abstract method
+//	abstract void details();//here we get compile time error
+	public static void main(String[] args) {
+		
+	}
+	
+/*	Explanation:-
+	 we Cannot create an abstract method inside a normal class.
+	 If we declare a method as abstract then we have to declare the class as 
+	 abstract that is mandatory but if wee declare the class as abstract then it is not mandatory to declare
+	 the method as abstract
+*/
+}
