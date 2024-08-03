09a38099e5403161af05859bd3d64967f45f8ea8
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-03 09:36:35 +0530

    Abstraction_4 program added successfuly

---
+++ java_test/Abstraction_4.java
@@ -0,0 +1,13 @@
+package java_test;
+
+// This program is used to demonstrate that can we decalre an abstract method as static
+public abstract class Abstraction_4 {
+
+//	 Creating an abstract method
+//	abstract static void detatis();//Here we get compile time error
+
+	/*
+	 * Explanation:- Abstract method are create to override by other class and as we
+	 * know a static method cannot be overridden
+	 */
+}
