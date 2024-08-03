95a92ed328ec1ae99f218e02b3bb12a876100ee1
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-03 09:42:37 +0530

    Interface_2 program added successfuly

---
+++ java_test/Interface_2.java
@@ -0,0 +1,13 @@
+package java_test;
+// This program is used demonstrate that can we provide any implementation to method inside the interface
+interface Interface_2 {
+   //creating the method details
+/*	public void details() {
+		System.out.println("Hello there ! i am the method inside an interface");
+	}//here we get compile time error
+*/
+/*	Explanation:-
+	When we create a method inside an interface it is bydefault declare as public and abstract.
+	And abstract method doesn't have body part.For that reason we cannot create concrete method inside an interface.
+*/
+}
