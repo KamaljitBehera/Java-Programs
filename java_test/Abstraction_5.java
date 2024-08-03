51dca72e18116e5b0afeda5c2842c2e3394c5d9a
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-03 09:39:54 +0530

    Abstraction_5 program added successfuly

---
+++ java_test/Abstraction_5.java
@@ -0,0 +1,12 @@
+package java_test;
+
+//This program is used to demonstrate can we declare an abstract method as final
+public abstract class Abstraction_5 {
+	// creating an abstract method
+//	abstract final void details();//here we get compile time error
+
+	/*
+	 * Explanation:- We cannot declare the abstract method as final because final
+	 * method ia also cannot be overridden.
+	 */
+}
