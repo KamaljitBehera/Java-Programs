95fa62a96190468b7e77aad48c6e93e7a97e5abf
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-03 09:42:11 +0530

    Interface_1 program added successfuly

---
+++ java_test/Interface_1.java
@@ -0,0 +1,19 @@
+package java_test;
+// This program is used to demonstrate that can we create object of an interface or not
+
+//Creating an interface named Test
+interface Test3 {
+	public void details();
+}
+
+//Driver class
+public class Interface_1 {
+	public static void main(String[] args) {
+//	Creating the object of Test3
+//	  Test3 ref=new Test3();//here we get compile time error
+
+		/*
+		 * Explanation:- We can not create an object of interface
+		 */
+	}
+}
