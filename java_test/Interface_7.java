f0faa79d4a82ee5466009116d6cb670ad1782a91
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-04 11:37:55 +0530

    Interface_7 added successfuly

---
+++ java_test/Interface_7.java
@@ -0,0 +1,36 @@
+package java_test;
+
+//this program is used to demonstrate that how to implement a method contained by two different interface having primitive type as an argument
+// Creating interface 1
+interface ClassParent1 {
+	// Creating method
+	public int details();
+}
+
+//Creating interface 2
+interface ClassParent2 {
+	// Creating same method
+	public float details();
+}
+
+//implemented class
+public class Interface_7 // implements ClassParent1 ,ClassParent2
+{
+	public static void main(String[] args) {
+//    	creating the object of implemented class
+		Interface_7 obj = new Interface_7();
+//    	obj.details(10);
+		/*
+		 * Explanation:- If more than one interface having same method signature but
+		 * different return type then it is impossible to provide the implementation
+		 */
+	}
+
+	/*
+	 * public int details() {
+	 * System.out.println("Hello there i am a method with float return type");
+	 * }//here we get compile time error public float details() {
+	 * System.out.println("Hello there i am a method with float return type");
+	 * }//here we get compile time error
+	 */
+}
