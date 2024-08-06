b657b549ed4443f9d29ce32eb85d8a75543c8a99
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-06 09:15:47 +0530

    Method_Overriding_4 program added successfuly

---
+++ java_test/Method_Overriding_4.java
@@ -0,0 +1,28 @@
+package java_test;
+
+// This program is used to demonstrate can we override the private method or not
+//Create the parent class
+abstract class P_Method_Overriding_4 {
+//	create the private method
+//	private abstract void add(int num1,int num2);// Here we get compile time error
+}
+
+//Driver class
+public class Method_Overriding_4 extends P_Method_Overriding_4 {
+// override the add method
+	private void add(int num1, int num2) {
+
+		System.out.println("The addition of two number is " + (num1 + num2));
+	}
+
+	public static void main(String[] args) {
+//		creating the object of that class
+		Method_Overriding_4 obj = new Method_Overriding_4();
+		obj.add(10, 20);
+
+		/*
+		 * Explanation:- Because we can't access the private data members out side the
+		 * class area or another class
+		 */
+	}
+}
