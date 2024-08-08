07f397511917d931aa6c024a29d5e7654ec3e435
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-08 09:08:47 +0530

    VarArgumentMethod Program add successfully

---
+++ java_programs/VarArgumentMethod.java
@@ -0,0 +1,31 @@
+package java_programs;
+//This program is demonstrate about the var-argument method and how to use it
+import java.util.Arrays;
+
+public class VarArgumentMethod {
+    // creating the var-argument method
+    public void printNumber(int x, int... y) {
+        // print the first number
+        System.out.println("The first number is " + x);
+
+        // print the remaining numbers
+        System.out.print("The remaining numbers are: ");
+        System.out.println(Arrays.toString(y));
+    }
+
+    // creating the main method
+    public static void main(String[] args) {
+        // creating the object of that class
+        VarArgumentMethod obj = new VarArgumentMethod();
+
+        // calling the var-argument method by passing different number of argument
+
+        obj.printNumber(20, 10);
+
+        obj.printNumber(20, 10, 30);
+
+        obj.printNumber(20, 10, 34, 50);
+
+        obj.printNumber(20, 10, 52, 59, 56, 25);
+    }
+}
