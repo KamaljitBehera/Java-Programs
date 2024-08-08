6e95eca7d8f97a6ff8988f8b9ec9ffd7b1a4d984
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-08 09:11:02 +0530

    Interface_11 Program add successfully

---
+++ java_test/Interface_11.java
@@ -0,0 +1,13 @@
+package java_test;
+//Java program that demonstrates why
+//interface can not have a constructor
+
+public interface Interface_11{
+/*	public Interface_11() {
+		System.out.println("Hello There!");
+	}
+*/
+	
+	/*	Explanation:-
+	Here we get compile time error because in interface we can't create the constructor.*/
+}
