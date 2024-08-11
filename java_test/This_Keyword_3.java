631f674561423feace28c759b4c916d5e45fbefd
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-11 10:38:31 +0530

    This_Keyword_3 Program add successfully

---
+++ java_test/This_Keyword_3.java
@@ -0,0 +1,24 @@
+package java_test;
+//This program is used to demonstrate how to use this key word as method argument
+public class This_Keyword_3 {
+//    creating some instance variable
+	String city="Bhubaneswar";
+	String state="Odisha";
+	
+//	create a method that take object as an argument
+	public void display(This_Keyword_3 obj) {
+		System.out.println("The city is "+obj.city+"The state is "+obj.state);
+	}
+//	create a method that call the display method by passing this keyword as an argument
+	public void get(){
+		display(this);
+	}
+	
+//	creating the main method
+	public static void main(String[] args) {
+//		creating the object
+		This_Keyword_3 ref=new This_Keyword_3();
+//		call the get method
+		ref.get();
+	}
+}
