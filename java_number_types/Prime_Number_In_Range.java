ae64107276bf93fe24c039cfa93d5b8e584d5fc7
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-08-18 21:30:47 +0530

    Prime_Number_In_Range is added successfully

---
+++ java_number_types/Prime_Number_In_Range.java
@@ -0,0 +1,31 @@
+package java_number_types;
+//this is the program to get the prime number between in a range.
+import java.util.Scanner;
+
+public class Prime_Number_In_Range {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//taking input from user
+		System.out.println("Enter the range::");
+		int range=sc.nextInt();
+		int j;
+		for(int i=1;i<=range;i++) {
+			int count=0;
+			////using for loop divide the input number with all the number from zero to that number
+			for(j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			///checking that the count is equal to 2 or not
+			if(count==2) {
+				System.out.print(i+" ");
+			}
+			
+		}
+//		closing the costly resources
+		sc.close();
+	}
+}
