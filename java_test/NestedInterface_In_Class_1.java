4bf1f394f53c7f5ab63cb7306f2dbdc30d19c08c
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-03 23:24:02 +0530

    NestedInterface_In_Class_1 is added successfuly

---
+++ java_test/NestedInterface_In_Class_1.java
@@ -0,0 +1,35 @@
+package java_test;
+//Program to check the condition that be applied on the nested members along with the public outer class.
+public class NestedInterface_In_Class_1 {
+//	Creating the inner interface with public access modifier
+	public interface InnerInterface_1{
+		static void status() {
+			System.out.println("Yes outer default and inner public modifier can acceptable");
+		}
+	}
+//	Creating the inner interface with default access modifier
+
+    interface InnerInterface_2{
+		static void status() {
+			System.out.println("Yes outer default and inner default modifier can acceptable");
+		}
+	}
+    
+//	Creating the inner interface with protected access modifier
+
+   protected interface InnerInterface_3{
+		static void status() {
+			System.out.println("Yes outer default and inner protcted modifier can  acceptable");
+		}
+	}
+
+    
+	//Creating the inner interface with private access modifier
+
+    private interface InnerInterface_4{
+		static void status() {
+			System.out.println("Yes outer default and inner private modifier can  acceptable");
+		}
+	}
+
+}
