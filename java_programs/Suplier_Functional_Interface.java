6d7e520a49b0bb4a8c0127e165b07460bc12a16e
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-04 23:35:42 +0530

    Suplier_Functional_Interface is added successfuly

--- java_programs/Suplier_Functional_Interface.java
+++ java_programs/Suplier_Functional_Interface.java
@@ -3,6 +3,8 @@ package java_programs;
 import java.util.function.Supplier;
 
 //Program is used to demonstrate how to use and implement the Suplier interface. 
+//Suplier interface is the functional interface which doesn't take any input but return any type of data.
+//And it has the method get().
 public class Suplier_Functional_Interface implements Supplier<Integer> {
 // Implemented by method overriding
 	@Override
@@ -13,7 +15,29 @@ public class Suplier_Functional_Interface implements Supplier<Integer> {
 	 
 //	creating the main
 	public static void main(String[] args) {
+//		Implementing using the anonymous class
+		Suplier_Functional_Interface SFI = new Suplier_Functional_Interface() {
+			public Integer get() {
+				System.out.println("I am implemented by anonymous class");
+				return 10;
+			}
+		};
 		
+//		Implementing by using the lambda expression
+		Supplier<Integer> obj=()->{
+				System.out.println("Implemented by the lambda expression.");
+			return 10;
+		};
+		
+//		Access all the ways of mplementation
+//		Calling the override method
+//		creating the object of class
+		Suplier_Functional_Interface ref = new Suplier_Functional_Interface();
+		ref.get();
+//		Calling the anonymous class
+		SFI.get();
+//		Calling the lambda expression
+		obj.get();
 	}
 	
 	
