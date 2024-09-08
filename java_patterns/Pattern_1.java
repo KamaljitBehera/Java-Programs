93fc79b70501ad4234baa6e69b326cb85aca0af2
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-08 21:59:22 +0530

    Pattern_1 is added successfuly

---
+++ java_patterns/Pattern_1.java
@@ -0,0 +1,28 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+ * 
+ * 1
+ * 22
+ * 333
+ * 4444
+ * 55555
+ * */
+public class Pattern_1 {
+//creating the main method
+		public static void main(String args[])
+		{
+//		declaring the loop variables
+		int i,j;
+//		outer loop is for count the row
+		for(i=1;i<=5;i++)
+		{
+//			inner loop is for print the column
+		for(j=1;j<=i;j++)
+		 {
+		System.out.print(i);
+		}
+		System.out.println();
+		}
+		}
+}
