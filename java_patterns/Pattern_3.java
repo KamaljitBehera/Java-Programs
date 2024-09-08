76d645cf4145ab32defe8445129afa10846ce0af
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-08 22:00:44 +0530

    Pattern_3 is added successfuly

---
+++ java_patterns/Pattern_3.java
@@ -0,0 +1,29 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+ * 
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * */
+public class Pattern_3 {
+//Creating the main method
+	public static void main(String args[])
+	{
+//		declaring the loop variables
+		int i,j;
+//		outer loop is for count the row
+		for(i=1;i<=5;i++)
+		{
+//			inner loop is for print the column
+		for(j=1;j<=i;j++)
+		 {
+		System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	}
+}
