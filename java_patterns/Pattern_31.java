20d0ec5311c5566beb8a76e2b4fba72a897d038a
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-13 21:24:49 +0530

    Pattern_31 is added successfuly

---
+++ java_patterns/Pattern_31.java
@@ -0,0 +1,29 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  *     1
+  *    21
+  *   321
+  *  4321
+  * 54321
+ * */
+public class Pattern_31 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j,k;
+//	Outer loop is for row
+	for(i=1;i<=5;i++)
+	{
+//  Inner loop is for column
+	for(k=1;k<=5-i;k++)
+	System.out.print(" ");
+	for(j=i;j>=1;j--)
+	{
+	System.out.print(j);
+	}
+	System.out.println();
+	}
+	}
+}
