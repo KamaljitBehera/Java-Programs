aa14c574b13adcdd0d67e6b491506419e020e669
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-15 00:02:11 +0530

    Pattern_40 is added successfuly

---
+++ java_patterns/Pattern_40.java
@@ -0,0 +1,28 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  * 55555    
+  * 4444
+  * 333
+  * 22
+  * 1
+ * */
+
+public class Pattern_40 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j;
+//	Outer loop is for row
+	for(i=5;i>=1;i--)
+	{
+//	Inner loop is for column
+	for(j=1;j<=i;j++)
+	{
+	System.out.print(i);
+	}
+	System.out.println();
+	}
+	}
+}
