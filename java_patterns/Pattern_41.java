1b1a8d974a07f78bc96c86c88d48456df43a2ccc
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-15 00:02:24 +0530

    Pattern_41 is added successfuly

---
+++ java_patterns/Pattern_41.java
@@ -0,0 +1,27 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  * 1010101010    
+  * 8888
+  * 666
+  * 44
+  * 2
+ * */
+public class Pattern_41 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j;
+//	Outer loop is for row
+	for(i=5;i>=1;i--)
+	{
+//	Inner loop is for column
+	 for(j=1;j<=i;j++)
+	{
+	System.out.print(i*2);
+	}
+	System.out.println();
+	}
+	}
+}
