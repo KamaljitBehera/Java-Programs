961f9b2c729abbcfbe1688a482ea631d24ae9508
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-19 22:39:24 +0530

    Pattern_50 is added successfuly

---
+++ java_patterns/Pattern_50.java
@@ -0,0 +1,27 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  * 12345    
+  * 1234
+  * 123
+  * 12
+  * 1
+ * */
+public class Pattern_50 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j;
+//	Outer loop is for row
+	for(i=5;i>=1;i--)
+	{
+//		Inner loop is for column
+	for(j=1;j<=i;j++)
+	{
+	System.out.print(j);
+	}
+	System.out.println();
+	}
+	}
+}
