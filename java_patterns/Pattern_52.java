67bb2bf36407ad40217111167bc2cbcbb52ff3ad
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-19 22:40:00 +0530

    Pattern_52 is added successfuly

---
+++ java_patterns/Pattern_52.java
@@ -0,0 +1,27 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  * 13579    
+  * 1357
+  * 135
+  * 13
+  * 1
+ * */
+public class Pattern_52 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j;
+//	Outer loop is for row
+	for(i=5;i>=1;i--)
+	{
+//	Inner loop is for column
+	for(j=1;j<=i;j++)
+	{
+	System.out.print((j*2)-1);
+	}
+	System.out.println();
+	}
+	}
+}
