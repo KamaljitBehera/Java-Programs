ba5496ccb8e7cb9f1f288be4032bcf044e7e6eed
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-09-19 22:39:42 +0530

    Pattern_51 is added successfuly

---
+++ java_patterns/Pattern_51.java
@@ -0,0 +1,27 @@
+package java_patterns;
+/*
+ * Program to print numbers in below pattern
+  * 246810    
+  * 2468
+  * 246
+  * 24
+  * 2
+ * */
+public class Pattern_51 {
+//	Creating main method
+	public static void main(String args[])
+	{
+//	Declaring the loop variables
+	int i,j;
+//	Outer loop is for row	
+	for(i=5;i>=1;i--)
+	{
+//	Inner loop is for column
+	for(j=1;j<=i;j++)
+	{
+	System.out.print(j*2);
+	}
+	System.out.println();
+	}
+	}
+}
