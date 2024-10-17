2019a3a0291acba5d13347c1e06a21c15749caf0
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-10-17 18:34:54 +0530

    program added successfully

---
+++ com/org/oops/var_argument_method/VarArgument_3.java
@@ -0,0 +1,22 @@
+package com.org.oops.var_argument_method;
+
+
+//This program is used to demonstrate can we pass multiple var-argument in a method
+public class VarArgument_3 {
+//	creating a method having two var-argument
+	
+/*	public void details(int... x,int...is y) {
+		System.out.println("I am var-argument method having two var-argument");
+	}//Here we get compile time error
+*/
+	
+//	creating the main method
+	public static void main(String[] args) {
+/*		Explanation:-
+
+		We can only provide a var-argument at the last of the argument Line.
+		And it should be one for a method.
+*/
+		}
+	
+}
