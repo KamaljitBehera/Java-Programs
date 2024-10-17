ab1ab1b474f61ff3269978b95cb61d07912ecdfa
KamaljitBehera <kamaljitbehera111@gmail.com>
2024-10-17 18:34:12 +0530

    program added successfully

---
+++ com/org/oops/var_argument_method/VarArgument_1.java
@@ -0,0 +1,19 @@
+package com.org.oops.var_argument_method;
+//This program is used to demonstrate some test case related to the var-argument method
+//case-1 can we put the var-argument as the first argument of the method
+public class VarArgument_1 {
+//	creating a var-argument method
+/*	public void details(int... a,int b) {
+		System.out.println("Hello there!");
+	}
+ * */
+	
+	
+//	creating the main method
+	public static void main(String[] args) {
+/*		Explanation:-
+		var-argument method can,t use as the first argument in any method because if that happens then no need to pass other argument it store all the pass
+		value in it.
+*/		
+	}
+}
